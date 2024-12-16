468a03ed8525d2bb2d8503c4deb4ed41e6266e7c
Andreina2 <andreinaarias5@gmail.com>
2024-12-16 17:19:11 +0100

    create Computers model

--- src/main/java/dev/andreina/ex_computers_shop/Computers.java
+++ src/main/java/dev/andreina/ex_computers_shop/Computers.java
@@ -1,5 +1,65 @@
 package dev.andreina.ex_computers_shop;
 
 public class Computers {
+    private String computerBrand;
+    private int memory;
+    private String processorCharacteristics;
+    private String operatingSystem;
+    private int price;
+
+    public Computers(String computerBrand, int memory, String processorCharacteristics, String operatingSystem,
+            int price) {
+        this.computerBrand = computerBrand;
+        this.memory = memory;
+        this.processorCharacteristics = processorCharacteristics;
+        this.operatingSystem = operatingSystem;
+        this.price = price;
+    }
+
+    public String getComputerBrand() {
+        return computerBrand;
+    }
+
+    public void setComputerBrand(String computerBrand) {
+        this.computerBrand = computerBrand;
+    }
+
+    public int getMemory() {
+        return memory;
+    }
+
+    public void setMemory(int memory) {
+        this.memory = memory;
+    }
+
+    public String getProcessorCharacteristics() {
+        return processorCharacteristics;
+    }
+
+    public void setProcessorCharacteristics(String processorCharacteristics) {
+        this.processorCharacteristics = processorCharacteristics;
+    }
+
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public void setOperatingSystem(String operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    
+
+    
+
+
 
 }
