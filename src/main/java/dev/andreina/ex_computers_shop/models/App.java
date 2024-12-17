b9ca714bd89c62d38575ce881a77280b1ef9a47d
Andreina2 <andreinaarias5@gmail.com>
2024-12-17 22:59:54 +0100

    modified class app

--- src/main/java/dev/andreina/ex_computers_shop/models/App.java
+++ src/main/java/dev/andreina/ex_computers_shop/models/App.java
@@ -1,4 +1,6 @@
 package dev.andreina.ex_computers_shop.models;
+import dev.andreina.ex_computers_shop.controller.StoreController;
+import dev.andreina.ex_computers_shop.view.StoreView;
 
 /**
  * Hello world!
@@ -12,6 +14,13 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //1: System.out.println("Hello World!");
+        Store store = new Store("Nina's Store", "Andreina Arias", 123456789);
+        
+        StoreController storeController= new StoreController(store); // Instanciando el controller
+
+        StoreView storeView= new StoreView(storeController);
+
+        storeView.menu();
     }
 }
